1689033794efab70e7ce0bd1adf8a5ced5a39550
beitaqa25 <mari01beita@gmail.com>
2024-09-30 06:38:25 +0000

    finished

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [color, setColor] = useState("red");
+    const [hasPurple, setHasPurple] = useState(false); // Estado para manejar si hay un color púrpura
+
+    const colors = ["red", "yellow", "green"];
+
+    const toggleColor = () => {
+        const currentIndex = colors.indexOf(color);
+        const nextIndex = (currentIndex + 1) % colors.length; // Cicla entre los colores
+        setColor(colors[nextIndex]);
+    };
+
+    const addPurple = () => {
+        setHasPurple(true); 
+        setColor("purple"); 
+    };
+
+    return (
+        <div className="traffic-container">
+            <div className="traffic-light">
+                <div 
+                    className={`light red ${color === "red" ? "selected" : ""}`} 
+                    onClick={() => setColor("red")}
+                ></div>
+                <div 
+                    className={`light yellow ${color === "yellow" ? "selected" : ""}`} 
+                    onClick={() => setColor("yellow")}
+                ></div>
+                <div 
+                    className={`light green ${color === "green" ? "selected" : ""}`} 
+                    onClick={() => setColor("green")}
+                ></div>
+
+                {hasPurple && (
+                    <div 
+                        className={`light purple ${color === "purple" ? "selected" : ""}`} 
+                        onClick={() => setColor("purple")}
+                    ></div>
+                )}
+            </div>
+
+            <div className="button-container">
+                <button onClick={toggleColor}>Toggle Color</button>
+                <button onClick={addPurple}>Add Purple</button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
